3452e489e75c4cdf8fd247f7dafefd615ee5dea1
MaxBatir <Mr.maxokoshow@gmail.com>
2024-12-04 21:02:34 +0300

    Add files via upload

---
+++ задача_2.py
@@ -0,0 +1,43 @@
+random_integer = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+result = int(input("Введите число, что бы подобрать пароль и не умереть:"))
+
+
+def password_for_live():
+    if result == 3:
+        print(12)
+    if result == 4:
+        print(1322)
+    if result == 5:
+        print(1423)
+    if result == 6:
+        print(121533)
+    if result == 7:
+        print(162534)
+    if result == 8:
+        print(13172635)
+    if result == 9:
+        print(1218273645)
+    if result == 10:
+        print(141923283746)
+    if result == 11:
+        print(11029384756)
+    if result == 12:
+        print(12131511124210394857)
+    if result == 13:
+        print(112211310495867)
+    if result == 14:
+        print(1611325212343114105968)
+    if result == 15:
+        print(1214114232133124115106978)
+    if result == 16:
+        print(1317115262143531341251161079)
+    if result == 17:
+        print(11621531441351261171089)
+    if result == 18:
+        print(12151811724272163631545414513612711810)
+    if result == 19:
+        print(118217316415514613712811910)
+    if result == 20:
+        print(13141911923282183731746416515614713812911)
+password_for_live()
